9ff7053a16dc6824020a7930252cb9b5c561b1fb
Sam Warnick <sam@warnick.me>
2025-08-15 07:26:25 -0400

    Try some vorder things

--- src/lib/components/MediaSelector.svelte
+++ src/lib/components/MediaSelector.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 	import { comparison } from '$lib/store.svelte';
 	import Poster from '$lib/components/Poster.svelte';
+	import z from 'zod';
+	import { page } from '$app/state';
+
+	const schema = z.enum(['first', 'second']).catch('first');
+	const searchParams = $derived(page.url.searchParams);
+	const forWhich = $derived.by(() => schema.parse(searchParams.get('for') ?? 'first'));
 </script>
 
 <div class="media-selector">
-	<a href="/search?for=first">
+	<a
+		href="/search?for=first"
+		class:focused={!comparison.first && !comparison.second && forWhich === 'first'}
+	>
 		<Poster media={comparison.first} />
 	</a>
-	<a href="/search?for=second">
+	<a
+		href={!comparison.first ? '/search?for=first' : '/search?for=second'}
+		class:focused={!comparison.first && !comparison.second && forWhich === 'second'}
+	>
 		<Poster media={comparison.second} />
 	</a>
 </div>
@@ -24,5 +36,13 @@
 	a {
 		/* width: min(200px, calc(50vw - 1rem - (var(--body-padding-inline) * 2))); */
 		width: 100%;
+	}
+
+	a.focused :global(.poster) {
+		--shadow-color: var(--uchu-blue-4-raw);
+		--shadow-strength: 70%;
+		box-shadow:
+			0 0 3px 2px oklch(var(--shadow-color) / calc(var(--shadow-strength) + 3%)),
+			0 0 5px 5px oklch(var(--shadow-color) / calc(var(--shadow-strength) + 5%));
 	}
 </style>
