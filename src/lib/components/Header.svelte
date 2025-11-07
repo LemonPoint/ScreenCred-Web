2eec17d5403cfd643d4a417e02568e4e436ffa22
Sam Warnick <sam@warnick.me>
2025-11-07 10:48:20 -0500

    Header logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,7 +4,7 @@
 
 <header>
 	<a href={resolve('/')}>
-		<h1>ScreenCred</h1>
+		<h1><span class="icon logo" style="--icon-size: 2em"></span>ScreenCred</h1>
 	</a>
 </header>
 
@@ -12,10 +12,19 @@
 	header {
 		background-color: var(--uchu-yin-9);
 		padding-inline: var(--body-padding-inline);
-		padding-block: 1rem;
+
+		h1 {
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+			line-height: 1;
+			font-weight: 500;
+			font-size: 1.5rem;
+		}
 
 		a {
 			color: white;
+			text-decoration: none;
 		}
 	}
 </style>
