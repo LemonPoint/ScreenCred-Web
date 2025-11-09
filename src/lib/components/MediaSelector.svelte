7cca03799b2fb26778feb3bd469852cbb586b6fe
Sam Warnick <sam@warnick.me>
2025-11-08 19:16:45 -0500

    turn links into buttons

--- src/lib/components/MediaSelector.svelte
+++ src/lib/components/MediaSelector.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
 	import { comparison } from '$lib/store.svelte';
 	import Poster from '$lib/components/Poster.svelte';
-	import z from 'zod';
-	import { page } from '$app/state';
+	import SearchInput from '$lib/components/SearchInput.svelte';
 
 	const { small = false } = $props();
 
-	const schema = z.enum(['first', 'second']).nullable();
-	const searchParams = $derived(page.url.searchParams);
-	const forWhich = $derived.by(() => schema.parse(searchParams.get('for')));
+	function startSearch(forWhich: 'first' | 'second') {
+		console.log('start search', forWhich);
+	}
 </script>
 
 <div class="media-selector" class:small>
-	<a href="/search?for=first" class:focused={forWhich === 'first'}>
+	<button onclick={() => startSearch('first')}>
 		<Poster media={comparison.first} />
-	</a>
-	<a
-		href={!comparison.first ? '/search?for=first' : '/search?for=second'}
-		class:focused={forWhich === 'second'}
-	>
+	</button>
+	<button onclick={() => startSearch('second')}>
 		<Poster media={comparison.second} />
-	</a>
+	</button>
 </div>
+
+<dialog>
+	<SearchInput />
+</dialog>
 
 <style>
 	@keyframes scaleDown {
@@ -41,27 +41,12 @@
 		animation-timeline: scroll();
 		animation-range: 100px 350px;
 		transition: max-width 0.2s ease-out;
-
-		&.small {
-			max-width: 200px;
-		}
 	}
 
-	a {
-		width: 100%;
-		&:first-child {
-			justify-self: end;
-		}
-		&:last-child {
-			justify-self: start;
-		}
-	}
-
-	a.focused :global(.poster) {
-		--shadow-color: var(--uchu-blue-4-raw);
-		--shadow-strength: 70%;
-		box-shadow:
-			0 0 3px 2px oklch(var(--shadow-color) / calc(var(--shadow-strength) + 3%)),
-			0 0 5px 5px oklch(var(--shadow-color) / calc(var(--shadow-strength) + 5%));
+	button {
+		border: none;
+		background: none;
+		padding: 0;
+		cursor: pointer;
 	}
 </style>
