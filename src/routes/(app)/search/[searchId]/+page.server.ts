972a0a94d563a8289c50a9643b8f1db6cdd37931
Sam Warnick <sam@warnick.me>
2025-11-24 14:09:03 -0500

    Make sure comparison is not the same

--- src/routes/(app)/search/[searchId]/+page.server.ts
+++ src/routes/(app)/search/[searchId]/+page.server.ts
@@ -6,7 +6,14 @@ import { parseSearchId } from '$lib/utils';
 
 export const load = async ({ params }) => {
 	const comparison = parseSearchId(params.searchId);
+
 	if (comparison.first !== null && comparison.second !== null) {
+		if (
+			comparison.first.id === comparison.second.id &&
+			comparison.first.type === comparison.second.type
+		) {
+			redirect(307, '/');
+		}
 		const [first, second] = await Promise.all([
 			getDetails(MEDIA_TYPE_KEY[comparison.first.type], parseInt(comparison.first.id)),
 			getDetails(MEDIA_TYPE_KEY[comparison.second.type], parseInt(comparison.second.id))
