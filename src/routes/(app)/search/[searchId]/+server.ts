a4c94ade05d32d90ce121d3d30bb5925600d64b5
Sam Warnick <sam@warnick.me>
2025-11-24 16:30:47 -0500

    Add analytics

--- src/routes/(app)/search/[searchId]/+server.ts
+++ src/routes/(app)/search/[searchId]/+server.ts
@@ -1,20 +1,18 @@
 import { normalizeSearchId, parseSearchId } from '$lib/utils';
+import sql from '$lib/server/db';
 
 export async function POST({ params }) {
 	const searchId = params.searchId;
 	const comparison = parseSearchId(searchId);
 	if (comparison.first !== null && comparison.second !== null) {
-		const searchTimestamp = Date.now();
 		const normalizedSearchId = normalizeSearchId(
 			`${comparison.first.type}${comparison.first.id}`,
 			`${comparison.second.type}${comparison.second.id}`
 		);
-		// await db.query(
-		// 	`INSERT INTO search_analytics
-		//    (search_id, normalized_search_id, count, timestamp, indexed_search_id)
-		//    VALUES ($1, $2, $3, $4, $5)`,
-		// 	[searchId, normalizedSearchId, 1, searchTimestamp, searchId]
-		// );
+
+		await sql`INSERT INTO search_analytics
+		   (search_id, normalized_search_id)
+		   VALUES (${searchId}, ${normalizedSearchId})`;
 		return new Response(null, { status: 201 });
 	}
 	return new Response(null, { status: 400 });
