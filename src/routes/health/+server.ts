2a4c85b270250fb15a53a7b12dcbba057b287404
Sam Warnick <sam@warnick.me>
2026-01-15 14:59:25 -0500

    Intentional error

--- src/routes/health/+server.ts
+++ src/routes/health/+server.ts
@@ -1,5 +1,6 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export function GET() {
-	return json({ status: 'ok' });
+	// return json({ status: 'ok' });
+	throw error(500, 'Intentional failure');
 }
