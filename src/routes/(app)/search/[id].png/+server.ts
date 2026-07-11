1e4962e1d7b4bfd2aa9ac2bdde108c99908cb1de
Sam Warnick <sam@warnick.me>
2026-07-10 20:56:04 -0400

    Fix error logging

--- src/routes/(app)/search/[id].png/+server.ts
+++ src/routes/(app)/search/[id].png/+server.ts
@@ -7,10 +7,12 @@ import ky from 'ky';
 export async function GET({ params, locals }) {
 	const id = params.id;
 	try {
+		locals.logger.info(`${env.IMAGE_API}/${id}.png`);
 		return ky.get(`${env.IMAGE_API}/${id}.png`);
 	} catch (error) {
 		locals.logger.error('Failed to generate image', {
-			error,
+			error: error instanceof Error ? error.message : 'Unknown error',
+			stack: error instanceof Error ? error.stack : undefined,
 			id,
 			params: params.id.split('__')
 		});
