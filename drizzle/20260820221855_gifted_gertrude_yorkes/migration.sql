4140f7fc5a36f6e0095fee8ffb0e33321fb12101
Sam Warnick <sam@warnick.me>
2026-08-20 19:15:55 -0400

    Update migrations

--- drizzle/20260820221855_gifted_gertrude_yorkes/migration.sql
+++ drizzle/20260820221855_gifted_gertrude_yorkes/migration.sql
@@ -7,7 +7,12 @@ CREATE TABLE "search_analytics" (
 	"normalized_search_id" varchar(255) NOT NULL,
 	"created_at" timestamp DEFAULT now()
 );
+*/
 --> statement-breakpoint
-CREATE INDEX "idx_search_analytics_normalized_search_id" ON "search_analytics" USING btree ("normalized_search_id" text_ops);--> statement-breakpoint
+/*
+CREATE INDEX "idx_search_analytics_normalized_search_id" ON "search_analytics" USING btree ("normalized_search_id" text_ops);
+*/
+--> statement-breakpoint
+/*
 CREATE INDEX "idx_search_analytics_search_id" ON "search_analytics" USING btree ("search_id" text_ops);
 */
