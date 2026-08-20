6b0092da05cfd589acbd84bd4dcf431817bf76df
Sam Warnick <sam@warnick.me>
2026-08-20 16:57:47 -0400

    Use correct env

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,8 @@
-import 'dotenv/config';
+import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { relations } from './drizzle/relations';
 
-const db = drizzle(process.env.DATABASE_URL!, {
+const db = drizzle(env.DATABASE_URL!, {
 	relations
 });
 export default db;
